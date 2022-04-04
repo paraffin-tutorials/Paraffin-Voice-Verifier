1695fd6432154865bb2bd1a9914e93092a81e049
Parsa <parsa.firoozi55@gmail.com>
2022-04-04 22:25:35 +0430

    Update main.js

--- main.js
+++ main.js
@@ -3,47 +3,50 @@ const { Client, Intents } = require('discord.js');
 const voiceDiscord = require(`@discordjs/voice`);
 const { createAudioPlayer, createAudioResource, NoSubscriberBehavior } = require('@discordjs/voice');
 
-const client = new Client({ intents: 
-  [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_VOICE_STATES
-  ] });
+const client = new Client(
+    { 
+        intents:
+        [
+            Intents.FLAGS.GUILDS,
+            Intents.FLAGS.GUILD_VOICE_STATES
+        ] 
+    });
 
-client.on('ready', () => 
+client.on('ready', () =>
 {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('voiceStateUpdate', async (OldVoiceState, NewVoiceState) =>
 {
     const guild = client.guilds.cache.get('796767783354368030');
     const connection = voiceDiscord.joinVoiceChannel(
-      {
-        channelId: '959872637058842674',
-        guildId: '796767783354368030',
-        adapterCreator: guild.voiceAdapterCreator,
-        selfDeaf: true,
-    });
+        {
+            channelId: '959872637058842674',
+            guildId: '796767783354368030',
+            adapterCreator: guild.voiceAdapterCreator,
+            selfDeaf: true,
+        });
     const player = createAudioPlayer(
-      {
-        behaviors:
-          {
-            noSubscriber: NoSubscriberBehavior.Pause,
-          }
-      });
-  
+        {
+            behaviors:
+                {
+                    noSubscriber: NoSubscriberBehavior.Pause,
+                }
+        });
+
     if (NewVoiceState.channel && NewVoiceState.member.user.id !== '743740869773099058')
     {
-            const role = await NewVoiceState.guild.roles.cache.find((Role) => Role.name === '< Verified Member />');
-          
-            const resource = createAudioResource('./rulesVoice.m4a');
-          
-            player.play(resource);  
-            connection.subscribe(player);
+        const role = await NewVoiceState.guild.roles.cache.find((Role) => Role.name === '< Verified Member />');
 
-            await NewVoiceState.member.roles.add(role);
+        const resource = createAudioResource('./rulesVoice.m4a');
 
-            console.log(`Member: ${NewVoiceState.member.user.tag} connected to ${NewVoiceState.channel.name}.`);
+        player.play(resource);
+        connection.subscribe(player);
+
+        await NewVoiceState.member.roles.add(role);
+
+        console.log(`Member: ${NewVoiceState.member.user.tag} connected to ${NewVoiceState.channel.name}.`);
     }
 });
 
